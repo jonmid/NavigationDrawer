785a49923302916a4a7a41f647883adc61207fa8
JonMid <jamideros@gmail.com>
2018-03-13 17:04:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jonmid/navigationdrawer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jonmid.navigationdrawer;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
